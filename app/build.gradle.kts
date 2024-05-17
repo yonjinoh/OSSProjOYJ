5e3b2cc171894009e8157355b3545e9c8ad9dd9b
YeRimmm-Cho <em514@dgu.ac.kr>
2024-05-17 18:22:05 +0900

    Refactoring: 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,16 @@ dependencies {
     // 추가한 부분
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation ("com.google.android.material:material:1.4.0")
-<<<<<<< HEAD
+
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-=======
-    implementation(libs.androidx.recyclerview)
->>>>>>> 7cd6f7f4ebc42213ef8cfa251f9102a5b07bd01a
+
+
+    implementation ("com.google.android.material:material:1.4.0")
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+    //implementation(libs.androidx.recyclerview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
