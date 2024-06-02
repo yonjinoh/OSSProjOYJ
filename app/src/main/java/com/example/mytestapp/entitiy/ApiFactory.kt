550ba9057adf324fb448cfc664fa01bdd76ff610
SuHyunKKim <kimsteven728@dgu.ac.kr>
2024-06-02 21:51:03 +0900

    ec2 링크 수정

--- app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
+++ app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 object ApiFactory {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("ec2-13-124-159-83.ap-northeast-2.compute.amazonaws.com:8000/")
+            .baseUrl("http://ec2-13-124-159-83.ap-northeast-2.compute.amazonaws.com:8000/")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
