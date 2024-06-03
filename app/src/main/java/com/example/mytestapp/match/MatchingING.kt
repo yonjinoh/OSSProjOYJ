1af7115fbba734287b3dfbc6ae70e7da1225b696
YeRimmm-Cho <em514@dgu.ac.kr>
2024-06-03 22:56:41 +0900

    Front: MatchinING 연결

--- app/src/main/java/com/example/mytestapp/match/MatchingING.kt
+++ app/src/main/java/com/example/mytestapp/match/MatchingING.kt
@@ -1,12 +1,29 @@
 package com.example.mytestapp.match
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.mytestapp.R
 
 class MatchingING : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matching_ing)
+
+        // 1.5초 후에 MatchingFragment로 전환
+        Handler(Looper.getMainLooper()).postDelayed({
+            replaceFragment(MatchingFragment())
+        }, 1500)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
