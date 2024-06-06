c0b5dd02b80859096bdf1d063f88f5aa64c42e6f
yonjinoh <2020110869@dgu.ac.kr>
2024-06-06 18:49:43 +0900

    ADD : Kiri3 가상환경 추가

--- app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
+++ app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 object ApiFactory {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://cdc7-128-134-0-90.ngrok-free.app")
+            .baseUrl("https://a26f-128-134-0-90.ngrok-free.app")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
