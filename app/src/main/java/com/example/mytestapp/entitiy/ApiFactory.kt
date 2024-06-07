f5d4122013b7bb38a9f7c1c42948293b476c6254
SuHyunKKim <kimsteven728@dgu.ac.kr>
2024-06-07 16:20:55 +0900

    ec2 배포 준비

--- app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
+++ app/src/main/java/com/example/mytestapp/entitiy/ApiFactory.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 object ApiFactory {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://726e-210-94-220-228.ngrok-free.app")
+            .baseUrl("https://ec2-13-124-159-83.ap-northeast-2.compute.amazonaws.com:8000")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
