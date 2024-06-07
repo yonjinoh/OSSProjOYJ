203cb6735f10db4ebccd3e2de580a4426d490939
SuHyunKKim <kimsteven728@dgu.ac.kr>
2024-06-07 17:05:57 +0900

    backend : ec2 https 접근 작업중

--- app/src/main/java/com/example/mytestapp/websocket/WebsocketManager.kt
+++ app/src/main/java/com/example/mytestapp/websocket/WebsocketManager.kt
@@ -20,7 +20,7 @@ class WebSocketManager(
 
     companion object {
         private const val NORMAL_CLOSURE_STATUS = 1000
-        private const val WEBSOCKET_URL = "ws://10.0.2.2:8000/ws/chat/"
+        private const val WEBSOCKET_URL = "wss://13.124.159.83/ws/chat/"
     }
 
     fun connect() {
